14630646e22fb95b50ae0b7d7a1e9282945d1523
[Kevin Jordan] <kevinjordan199449@gmail.com>
2022-07-03 11:46:34 -0400

    Start work on new portfolio website

---
+++ app.js
@@ -0,0 +1,16 @@
+`use strict`;
+
+const express = require(`express`);
+const bodyParser = require(`body-parser`);
+const ejs = require(`ejs`);
+
+const app = express();
+
+app.set(`view engine`, `ejs`);
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.render(`home`);
+});
+
+app.listen(3000);
